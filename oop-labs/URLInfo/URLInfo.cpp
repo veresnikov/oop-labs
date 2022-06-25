7aad1d629694813a696a71ff6b462640dc00bf3d
Kirill Veresnikov <kirill.veres@list.ru>
2022-06-25 19:46:08 +0300

    Добавил консольное приложение для вывода информации о урлах

---
+++ oop-labs/URLInfo/URLInfo.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "../URL/HTTPUrl.h"
+
+int main()
+{
+	std::string stringURL;
+	while (std::cin >> stringURL)
+	{
+		std::cout << stringURL << std::endl;
+		try
+		{
+			url::HTTPUrl url(stringURL);
+			std::cout << "protocol: " << url::protocols::ProtocolToString(url.GetProtocol()) << std::endl
+					  << "domain: " << url.GetDomain() << std::endl
+					  << "port: " << url.GetPort() << std::endl
+					  << "document: " << url.GetDocument() << std::endl
+					  << "url: " << url.GetURL() << std::endl;
+		}
+		catch (const std::exception& ex)
+		{
+			std::cerr << ex.what() << std::endl;
+		}
+	}
+}
